cd5cf317dc3d42886f10ea7da02d644b6e84a00d
benhayehudi <bengreenberg@gmail.com>
2021-03-08 14:04:26 +0200

    add comments to client

--- lib/dev_orbit/client.rb
+++ lib/dev_orbit/client.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
+# Create a client to log DEV interactions in your Orbit workspace
+# Credentials can either be passed in to the instance or be loaded
+# from environment variables
+#
+# @example
+#   client = DevOrbit::Client.new
+#
+# @option params [String] :dev_username
+#   The username of the person or organization to fetch DEV interactions for
+#
+# @option params [String] :dev_api_key
+#   The API key for the DEV API
+#
+# @option params [String] :orbit_workspace
+#   The workspace ID for the Orbit workspace
+#
+# @option params [String] :orbit_api_key
+#   The API key for the Orbit API
+#
+# @param [Hash] params
+#
+# @return [DevOrbit::Client]
+#s
 class DevOrbit::Client
   attr_accessor :dev_username, :dev_api_key, :orbit_workspace, :orbit_api_key
 
@@ -10,6 +33,7 @@ class DevOrbit::Client
     @dev_username ||= params.fetch(:dev_username, ENV['DEV_USERNAME'])
   end
 
+  # Fetch new comments from DEV and post them to the Orbit workspace
   def comments
     DevOrbit::Dev.new(api_key: @dev_api_key, username: @dev_username).process_comments
   end
