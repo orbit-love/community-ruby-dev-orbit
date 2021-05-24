f503e21a3cad02bd316c79c09ec3e8128f7885e3
Ben Greenberg <ben@orbit.love>
2021-05-24 18:49:27 +0300

    move to activities gem

--- lib/dev_orbit/interactions/comment.rb
+++ lib/dev_orbit/interactions/comment.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "net/http"
 require "json"
 require "action_view"
 require_relative "../utils"
@@ -22,23 +21,13 @@ module DevOrbit
       end
 
       def after_initialize!
-        url = URI("https://app.orbit.love/api/v1/#{@workspace_id}/activities")
-
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        req = Net::HTTP::Post.new(url)
-        req["Accept"] = "application/json"
-        req["Content-Type"] = "application/json"
-        req["Authorization"] = "Bearer #{@api_key}"
-        req["User-Agent"] = "community-ruby-dev-orbit/#{DevOrbit::VERSION}"
-
-        req.body = construct_body
-
-        req.body = req.body.to_json
-
-        response = http.request(req)
-
-        JSON.parse(response.body) if DevOrbit::Utils.valid_json?(response.body)
+        OrbitActivities::Request.new(
+          api_key: @api_key,
+          workspace_id: @workspace_id,
+          user_agent: "community-ruby-dev-orbit/#{DevOrbit::VERSION}",
+          action: "new_activity",
+          body: construct_body.to_json
+        )
       end
 
       def construct_body
