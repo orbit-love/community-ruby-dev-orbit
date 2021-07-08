04e4a776b9bb6cd9be089003365d9d432fda6eeb
Ben Greenberg <ben@orbit.love>
2021-07-08 13:55:15 +0300

    pagination and other improvements

--- lib/dev_orbit/orbit.rb
+++ lib/dev_orbit/orbit.rb
@@ -6,13 +6,14 @@ require "active_support/time"
 
 module DevOrbit
   class Orbit
-    def initialize(type:, data:, workspace_id:, api_key:, historical_import: false)
+    def initialize(type:, data:, workspace_id:, api_key:, historical_import: false, last_orbit_member_timestamp: nil)
       @type = type
       @data = data
       @workspace_id = workspace_id
       @api_key = api_key
       @historical_import = historical_import
       @last_orbit_activity_timestamp = last_orbit_activity_timestamp(type)
+      @last_orbit_member_timestamp = last_orbit_member_timestamp
     end
     
     def call
@@ -21,11 +22,11 @@ module DevOrbit
         
         @data[:comments].each do |comment|
           unless @historical_import && @last_orbit_activity_timestamp
-            next if comment["created_at"] || comment[:created_at] < @last_orbit_activity_timestamp unless @last_orbit_activity_timestamp.nil? 
+            next if (comment["created_at"] || comment[:created_at]) < @last_orbit_activity_timestamp unless @last_orbit_activity_timestamp.nil? 
           end
 
           if @last_orbit_activity_timestamp && @historical_import == false
-            next if comment["created_at"] || comment[:created_at] < @last_orbit_activity_timestamp
+            next if (comment["created_at"] || comment[:created_at]) < @last_orbit_activity_timestamp
           end
 
           times += 1
@@ -46,16 +47,38 @@ module DevOrbit
       end
 
       if @type == "followers"
-        DevOrbit::Interactions::Follower.new(
-          id: @data[:follower]["id"],
-          name: @data[:follower]["name"],
-          username: @data[:follower]["username"],
-          url: @data[:follower]["path"],
-          workspace_id: @workspace_id,
-          api_key: @api_key
-        )
+        counter = 0
+        @data[:followers].each do |follower|
+          next if follower.nil? || follower.empty?
+
+          unless @historical_import && @last_orbit_member_timestamp
+            next if follower["created_at"] < @last_orbit_member_timestamp unless @last_orbit_member_timestamp.nil?
+          end
+  
+          if @last_orbit_member_timestamp && @historical_import == false
+            next if follower["created_at"] < @last_orbit_member_timestamp
+          end
+
+          sleep(20) if counter % 100 == 0 && counter != 0 # API rate limiting
+
+          DevOrbit::Interactions::Follower.new(
+            id: follower["id"],
+            name: follower["name"],
+            username: follower["username"],
+            url: follower["path"],
+            workspace_id: @workspace_id,
+            api_key: @api_key
+          )
+          counter += 1
+        end
+        output = "Sent #{counter} new followers to Orbit"
+
+        puts output
+        return output
       end
     end
+
+    private
 
     def last_orbit_activity_timestamp(type)
       @last_orbit_activity_timestamp ||= begin
